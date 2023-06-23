5a6eb33989d999e79fa30f0093a87d35e7b8cc8b
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-06-23 10:20:20 -0400

    second commit

--- contracts/tacoin.sol
+++ contracts/tacoin.sol
@@ -1,10 +1,10 @@
  // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity >=0.8.9 <0.8.20;
 
-import "eth_contracts/tafit_coin/contracts/token/ERC20/ERC20.sol";
-import "eth_contracts/tafit_coin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "eth_contracts/tafit_coin/contracts/security/Pausable.sol";
-import "eth_contracts/tafit_coin/contracts/access/Ownable.sol";
+import "tafit_coin/contracts/token/ERC20/ERC20.sol";
+import "tafit_coin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "tafit_coin/contracts/security/Pausable.sol";
+import "tafit_coin/contracts/access/Ownable.sol";
 
 contract tacoin is ERC20, ERC20Burnable, Pausable, Ownable {
     constructor() ERC20("tacoin", "TTSC") {}
