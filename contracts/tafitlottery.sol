f5e7d6eeea8e9cf0fd4613b446dc0a3cf463fa49
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-06-23 15:45:34 -0400

    second commit (lottery fee)

--- contracts/tafitlottery.sol
+++ contracts/tafitlottery.sol
@@ -3,5 +3,19 @@ pragma solidity >=0.8.9 <0.8.20;
 
 
 contract tafitLottery{
- 
+ //entities - manager, players and winner
+ address private  manager;
+ address payable[] public players;
+ address payable  public winner;
+
+    constructor(){
+        manager==msg.sender;
+    }  
+
+    function toPlay() public payable{
+        require(msg.value==0.001 ether, "Please pay .001 ether to play ");
+        players.push(payable(msg.sender));
+    }
+    
+
 }
